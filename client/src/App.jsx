a8b1710b28ede2d119b985e5826884a1266d5e89
k-tech19 <kennyjean561@gmail.com>
2022-07-07 20:57:51 -0400

    Printing data on page successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,68 @@
-import { useState,useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
-import API_KEY from "./gitignore/API_KEY";
 import axios from "axios";
 
 
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid='
-const API_key = API_KEY;
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid="
+// const API_KEY = process.env.REACT_APP_API_KEY
 
 function App() {
 
-  const [forecast,setForecast] = useState('')
+  const [humidity, setHumidity] = useState('')
+  const [pressure, setPressure] = useState('')
+  const [city,setCity] = useState('')
+  const [country,setCountry] = useState('')
+  const [temp,setTemp] = useState('')
+  const [temp_min,setTempMin] = useState('')
+  const [temp_max,setTempMax] = useState('')
+  const [description,setDescription] = useState('')
+  const [icon,setIcon] = useState('')
+  const [condition,setCondition] = useState('')
+
+
+
 
 
 
   useEffect(()=>{
     axios
-    .get(`${baseURL}${API_key}`)
+    .get(`${baseURL}${API_KEY}`)
     .then((response)=>{
-      setForecast(response.data.main.temp)
-      console.log(response.data.main)
+      setHumidity(response.data.main.humidity)
+      setPressure(response.data.main.pressure)
+      setCity(response.data.name)
+      setCountry(response.data.sys.country)
+      setTemp(response.data.main.temp)
+      setTempMin(response.data.main.temp_min)
+      setTempMax(response.data.main.temp_max)
+      setDescription(response.data.weather[0].description)
+      setIcon(response.data.weather[0].icon)
+      setCondition(response.data.weather[0].main)
+      console.log(response.data)
     })
     .catch((err)=>{
       console.log(err)
     })
-  })
+  },[])
 
 
   return (
     <div className="App">
       <h1 class="text-3xl font-bold">Weather app!!</h1>
       <div>
-        {forecast}
+        <ul>
+          <li>Temperature: {temp}</li>
+          <li>Humidity: {humidity}</li>
+          <li>Pressure: {pressure}</li>
+          <li>Min-Temp: {temp_min}</li>
+          <li>Condition: {condition}</li>
+          <li>Max-Temp: {temp_max}</li>
+          <li>Description: {description}</li>
+          <li>Icon: {icon}</li>
+          <li>City: {city}</li>
+          <li>Country: {country}</li>
+        </ul>
+        <img src=""/>
       </div>
     </div>
   );
